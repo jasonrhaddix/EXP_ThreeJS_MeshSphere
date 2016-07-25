26eaf4eb309e565c8607882a86f523c89c8e6416
Jason Haddix <Jason Haddix>
2016-07-25 11:23:48 -0700

    Working final commit

--- builds/development/scripts/js/main.js
+++ builds/development/scripts/js/main.js
@@ -1,19 +1,14 @@
-
-
 var container;
 var camera, scene, renderer;
-
 var supports_WebGL;
-
-var SHADOW_MAP_WIDTH = 2048, SHADOW_MAP_HEIGHT = 1024;
-
+var SHADOW_MAP_WIDTH = 2048
+var SHADOW_MAP_HEIGHT = 1024;
 var light;
 var lightShadowMapViewer;
-
 var meshVertices = [];
 var meshPlane;
 var vertices;
-
+var jsonLoader;
 
 
 
@@ -62,18 +57,14 @@ function checkStatus_Features ()
 
 
 
-
 function initWorld_ThreeJS()
 {
     // Camera
-    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );
-    
+    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );    
     camera.position.x = 0;
     camera.position.y = 0;
     camera.position.z = -100;
-
     fovAlgorithm = 2 * Math.atan( ( (window.innerWidth) / camera.aspect ) / ( 2 * 1175 ) ) * ( 180 / Math.PI );
-
     camera.fov = fovAlgorithm;
     camera.updateProjectionMatrix();
 
@@ -92,24 +83,26 @@ function initWorld_ThreeJS()
 
 
     // Lights
-    // ---- Ambient Light
+    // ---- AmbientLight
     scene.add( new THREE.AmbientLight( 0xFFFFFF, 0.05 ) );
 
-
+    // ---- DirectionalLight 1
     directionalLight1 = new THREE.DirectionalLight( 0xFFFFFF, 0.5   );
     directionalLight1.position.set( 0, 0, 60 );
     directionalLight1.target.position.set( 0, 0, 100 );
     scene.add( directionalLight1 );
 
-    directionalLight = new THREE.DirectionalLight( 0xFF0099, 1.3 );
-    directionalLight.position.set( 0, -25, -5 );
-    directionalLight.target.position.set( 0, 0, 0 );
-    directionalLight.castShadow = true;
-    directionalLight.shadow = new THREE.LightShadow( new THREE.PerspectiveCamera( 10, 1, 1200, 2500 ) );
-    directionalLight.shadow.mapSize.width = SHADOW_MAP_WIDTH;
-    directionalLight.shadow.mapSize.height = SHADOW_MAP_HEIGHT;
-    scene.add( directionalLight ); 
+    // ---- DirectionalLight 2
+    directionalLight2 = new THREE.DirectionalLight( 0xFF0099, 1.3 );
+    directionalLight2.position.set( 0, -20, -5 );
+    directionalLight2.target.position.set( 0, 0, 0 );
+    directionalLight2.castShadow = true;
+    directionalLight2.shadow = new THREE.LightShadow( new THREE.PerspectiveCamera( 10, 1, 1200, 2500 ) );
+    directionalLight2.shadow.mapSize.width = SHADOW_MAP_WIDTH;
+    directionalLight2.shadow.mapSize.height = SHADOW_MAP_HEIGHT;
+    scene.add( directionalLight2 ); 
 
+    // ---- PointLight 1
     pointLight1 = new THREE.PointLight( 0xFFFFFF, 2, 30 );
     pointLight1.position.set(0, -25, 10);
     pointLight1.castShadow = true;
@@ -118,6 +111,7 @@ function initWorld_ThreeJS()
     pointLight1.shadow.mapSize.height = SHADOW_MAP_HEIGHT;
     scene.add( pointLight1 ); 
 
+    // ---- PointLight 2
     pointLight2 = new THREE.PointLight( 0xFFFFFF, 2, 80 );
     pointLight2.position.set(0, 25, -10);
     pointLight2.castShadow = true;
@@ -126,6 +120,7 @@ function initWorld_ThreeJS()
     pointLight2.shadow.mapSize.height = SHADOW_MAP_HEIGHT;
     scene.add( pointLight2 ); 
 
+    // ---- PointLight 3
     pointLight3 = new THREE.PointLight( 0xFFFFFF, 2, 80 );
     pointLight3.position.set(0, 25, 10);
     pointLight3.castShadow = true;
@@ -135,10 +130,12 @@ function initWorld_ThreeJS()
     scene.add( pointLight3 ); 
 
     
-    pointLightRep = new THREE.SphereGeometry( 2, 16, 6);    
-    lightMesh = new THREE.Mesh( pointLightRep, new THREE.MeshBasicMaterial( { color: 0x71E3FF, wireframe:true } ) );
-    lightMesh.position.copy( directionalLight.position );
+    // ---- Light Representation
+    lightRep = new THREE.SphereGeometry( 2, 16, 6);    
+    lightMesh = new THREE.Mesh( lightRep, new THREE.MeshBasicMaterial( { color: 0x71E3FF, wireframe:true } ) );
+    lightMesh.position.copy( directionalLight1.position );
     // scene.add( lightMesh );
+
 
     /*
     // Controls  
@@ -148,7 +145,11 @@ function initWorld_ThreeJS()
     controls.enableZoom = false;
     */
 
-    loadJSONMesh();
+
+    jsonLoader = new THREE.JSONLoader();
+
+
+    loadInnerGeom();
     
 }
 
@@ -157,9 +158,9 @@ function initWorld_ThreeJS()
 
 
 
-function loadJSONMesh()
+function loadInnerGeom()
 {
-    jsonLoader = new THREE.JSONLoader();
+    
     
     jsonLoader.load("./site-assets/sphere2.json", function ( geometry )
     {
@@ -200,42 +201,26 @@ function loadJSONMesh()
         var Mat2 = new THREE.MeshLambertMaterial({color: 0x5b6463, vertexColors: THREE.FaceColors, wireframe:true})
         meshPlane2 = new THREE.Mesh( geometry, Mat2 );
         meshPlane2.scale.set( 16, 16, 16 );
-         scene.add( meshPlane2 ); 
-
-/*
-        material = new THREE.PointsMaterial( { size: 35, sizeAttenuation: false,  transparent: true } );
-        material.color.setHSL( 1.0, 0.3, 0.7 );
-
-        particles = new THREE.Points( geometry, material );
-        particles.sortParticles = true;
-        scene.add( particles );
-*/
+        scene.add( meshPlane2 ); 
 
 
-        geometry.computeFaceNormals();
-
-        wireframe = new THREE.WireframeHelper( meshPlane, 0x6700b0 );
-        //scene.add( wireframe );
-
-
-
-        // initHeight();
-        // definePulse();
-        
-        
+        geometry.computeFaceNormals();     
 
     });
+    
+
+    loadMiddleGeom();    
+
+}
 
 
+
+
+function loadMiddleGeom()
+{
     jsonLoader.load("./site-assets/sphere2.json", function ( geometry )
     {
-        /*
-        meshPlane = THREE.SceneUtils.createMultiMaterialObject( geometry, [
-            new THREE.MeshLambertMaterial({ color : 0x6700b0, vertexColors : THREE.FaceColors, wireframe : true, opacity : 1 }),
-            new THREE.MeshLambertMaterial({ color : 0x35006b, vertexColors : THREE.FaceColors })
-        ]);
-        */
-                                                  /* 0x14cdbc */
+                                                    /* 0x14cdbc */
         var Mat3 = new THREE.MeshLambertMaterial({color:0x0E9185, vertexColors:THREE.VertexColors, wireframe:true})
         meshPlane3 = new THREE.Mesh( geometry, Mat3 );
         //meshPlane3.scale.set( 22, 22, 22 );
@@ -281,12 +266,20 @@ function loadJSONMesh()
             particles.sortParticles = true;
             scene.add( particles );
 
-        } );
+            } );
 
-});
+    });
+
+
+    loadOutterGeom();
+
+}
 
 
 
+
+function loadOutterGeom()
+{
     jsonLoader.load("./site-assets/sphere2.json", function ( geometry )
     {
         /*
@@ -343,7 +336,7 @@ function loadJSONMesh()
             {
                 threeJS_Animate();
             
-            }, 500 );
+            }, 10 );
         
 
             document.addEventListener( 'mousemove', onDocumentMouseDown, false );
@@ -355,11 +348,7 @@ function loadJSONMesh()
         } );
 
     });
-
 }
-
-
-
 
 
 
@@ -388,6 +377,8 @@ function threeJS_Render()
     meshPlane2.rotation.set( x, y, z ); 
     meshPlane3.rotation.set( y, z, x );
     particles.rotation.set( y, z, x ); 
+    
+    var y = motionRate * 0.6;
     meshPlane4.rotation.set( 0, y, 0 );
     particles2.rotation.set( 0, y, 0 ); 
 
@@ -466,25 +457,22 @@ function onDocumentMouseDown( event )
             i = 0;
             while( i < intersects.length )
             {
-                TweenMax.fromTo(faces[intersects[i].faceIndex].color, 0.5, {r : 10,g :10, b : 10},{r : 1,g :1, b : 1})  
-                 
-              //  TweenMax.fromTo(faces[intersects[i].faceIndex+1].color, 0.5,{r : 6,g :6, b : 6},{r : 1,g :1, b : 1})
-              //  TweenMax.fromTo(faces[intersects[i].faceIndex+2].color, 0.5, {r : 2,g :2, b : 2},{r : 1,g :1, b : 1}) 
-                
+                TweenMax.fromTo(faces[intersects[i].faceIndex].color, 0.75, {r : 10,g :10, b : 10},{r : 1,g :1, b : 1})  
+                   
                 try {    
-                    TweenMax.fromTo(faces[intersects[i].faceIndex+1].color, 0.5, {r : 4,g :4, b : 4},{r : 1,g :1, b : 1})   
+                    TweenMax.fromTo(faces[intersects[i].faceIndex+1].color, 0.75, {r : 4,g :4, b : 4},{r : 1,g :1, b : 1})   
                 } catch(error){}
 
                 try {    
-                    TweenMax.fromTo(faces[intersects[i].faceIndex+2].color, 0.5, {r : 2,g :2, b : 2},{r : 1,g :1, b : 1})          
+                    TweenMax.fromTo(faces[intersects[i].faceIndex+2].color, 0.75, {r : 2,g :2, b : 2},{r : 1,g :1, b : 1})          
                 } catch(error){}
 
                 try {    
-                    TweenMax.fromTo(faces[intersects[i].faceIndex-1].color, 0.5, {r : 4,g :4, b : 4},{r : 1,g :1, b : 1})   
+                    TweenMax.fromTo(faces[intersects[i].faceIndex-1].color, 0.75, {r : 4,g :4, b : 4},{r : 1,g :1, b : 1})   
                 } catch(error){}
 
                 try {    
-                    TweenMax.fromTo(faces[intersects[i].faceIndex-2].color, 0.5, {r : 2,g :2, b : 2},{r : 1,g :1, b : 1})          
+                    TweenMax.fromTo(faces[intersects[i].faceIndex-2].color, 0.75, {r : 2,g :2, b : 2},{r : 1,g :1, b : 1})          
                 } catch(error){}
 
 
@@ -501,9 +489,9 @@ function onDocumentMouseDown( event )
                     TweenMax.to( tempB, 0.3, { y : tempB.y + THREE.Math.randFloat( 0.01, 0.05 ), ease: Expo.easeOut } )
                     TweenMax.to( tempC, 0.3, { y : tempC.y + THREE.Math.randFloat( 0.01, 0.05 ), ease: Expo.easeOut } )
 
-                    TweenMax.to( tempA, 0.8, { y : tempA.originalY, delay: 0.5, ease: Elastic.easeOut } )
-                    TweenMax.to( tempB, 0.8, { y : tempB.originalY, delay: 0.5, ease: Elastic.easeOut } )
-                    TweenMax.to( tempC, 0.8, { y : tempC.originalY, delay: 0.5, ease: Elastic.easeOut } )
+                    TweenMax.to( tempA, 0.65, { y : tempA.originalY, delay: 0.5, ease: Elastic.easeOut } )
+                    TweenMax.to( tempB, 0.65, { y : tempB.originalY, delay: 0.5, ease: Elastic.easeOut } )
+                    TweenMax.to( tempC, 0.65, { y : tempC.originalY, delay: 0.5, ease: Elastic.easeOut } )
     
                 }
                
@@ -512,7 +500,6 @@ function onDocumentMouseDown( event )
             }      
 
         }
-
        
         mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
         mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;     
